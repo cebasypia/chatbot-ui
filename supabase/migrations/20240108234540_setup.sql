3479640d8082ec9261d972e43f327c3eede23836
cebasypia <cebasypia.tskm8@gmail.com>
2024-09-16 17:09:48 +0900

    supabase projectを設定

--- supabase/migrations/20240108234540_setup.sql
+++ supabase/migrations/20240108234540_setup.sql
@@ -50,8 +50,8 @@ LANGUAGE 'plpgsql'
 SECURITY DEFINER
 AS $$
 DECLARE
-  project_url TEXT := 'http://supabase_kong_chatbotui:8000';
-  service_role_key TEXT := 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImV4cCI6MTk4MzgxMjk5Nn0.EGIM96RAZx35lJzdJsyH-qQwv8Hdp7fsn3W0YpN81IU'; -- full access needed for http request to storage
+  project_url TEXT := 'https://figkvcwixpdevtpedjqa.supabase.co';
+  service_role_key TEXT := 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZpZ2t2Y3dpeHBkZXZ0cGVkanFhIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyNTkyNDkyNCwiZXhwIjoyMDQxNTAwOTI0fQ.0x0vOpDqLQ6Fu39aYmqI33lyffP5OxiH2-kiNJqE8VQ';
   url TEXT := project_url || '/storage/v1/object/' || bucket || '/' || object;
 BEGIN
   SELECT
